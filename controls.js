04b32adf6652324428345a8d2cab164677f1fe83
hnqcjw <hnqcjw.xvc@gmail.com>
2025-10-23 16:56:18 -0700

    Formatted code

--- controls.js
+++ controls.js
@@ -4,8 +4,16 @@ including kontra.js
 developed by hnqcjw
 */
 
-const { initKeys, keyPressed } = kontra;
-import { stuckup, stuckdown, stuckleft, stuckright } from './imgs.js';
+const {
+    initKeys,
+    keyPressed
+} = kontra;
+import {
+    stuckup,
+    stuckdown,
+    stuckleft,
+    stuckright
+} from './imgs.js';
 
 initKeys();
 
@@ -15,31 +23,30 @@ let ymap = 0;
 export let hp = 100;
 
 export const state = {
-  hp: 100
+    hp: 100
 };
 
 export function updateMovement() {
-  if (keyPressed('d') && stuckleft === 0) {
-    xmap = -10; 
-  } else if (keyPressed('a') && stuckright === 0) {
-    xmap = 10;
-  } else {
-    xmap = 0;
-  }
-  if (keyPressed('s') && stuckdown === 0) {
-    ymap = -10;
-  } else if (keyPressed('w') && stuckup === 0) {
-    ymap = 10;
-  }
-  else {
-    ymap = 0
-  }
+    if (keyPressed('d') && stuckleft === 0) {
+        xmap = -10;
+    } else if (keyPressed('a') && stuckright === 0) {
+        xmap = 10;
+    } else {
+        xmap = 0;
+    }
+    if (keyPressed('s') && stuckdown === 0) {
+        ymap = -10;
+    } else if (keyPressed('w') && stuckup === 0) {
+        ymap = 10;
+    } else {
+        ymap = 0
+    }
 }
 
 export function getXmap() {
-  return xmap;
+    return xmap;
 }
 
 export function getYmap() {
-  return ymap;
+    return ymap;
 }
--- extraStuff.js
+++ extraStuff.js
@@ -4,40 +4,38 @@ const audioElement2 = document.getElementById("sfx");
 let currentSfx = "";
 
 export function switchSong(newSong) {
-  if (audioElement.src.includes(newSong)) return;
-  audioElement.src = newSong;
-  audioElement.load();
-  audioElement.play().catch(() => {});
+    if (audioElement.src.includes(newSong)) return;
+    audioElement.src = newSong;
+    audioElement.load();
+    audioElement.play().catch(() => {});
 }
 
 const soundCache = {};
 
 export function playSound(soundFile) {
-  if (!soundCache[soundFile]) {
-    soundCache[soundFile] = new Audio(soundFile);
-  }
-  const sound = soundCache[soundFile];
-  sound.currentTime = 0;
-  sound.play().catch(() => {});
+    if (!soundCache[soundFile]) {
+        soundCache[soundFile] = new Audio(soundFile);
+    }
+    const sound = soundCache[soundFile];
+    sound.currentTime = 0;
+    sound.play().catch(() => {});
 }
 
 
 import * as gameState from './imgs.js';
 setInterval(() => {
-  if (gameState.battlemode === 1 && currentSong !== "FIGHT.ogg") {
-    switchSong("FIGHT.ogg");
-    currentSong = "FIGHT.ogg";
-  }
-  else if (gameState.battlemode === 0 && currentSong !== "chillFlux.ogg" && gameState.level === 20) {
-    switchSong("celebrate.ogg");
-    currentSong = "celebrate.ogg";
-  }
-  else if (gameState.battlemode === 0 && currentSong !== "chillFlux.ogg" && gameState.level !== 20) {
-    switchSong("chillFlux.ogg");
-    currentSong = "chillFlux.ogg";
-  }
+    if (gameState.battlemode === 1 && currentSong !== "FIGHT.ogg") {
+        switchSong("FIGHT.ogg");
+        currentSong = "FIGHT.ogg";
+    } else if (gameState.battlemode === 0 && currentSong !== "chillFlux.ogg" && gameState.level === 20) {
+        switchSong("celebrate.ogg");
+        currentSong = "celebrate.ogg";
+    } else if (gameState.battlemode === 0 && currentSong !== "chillFlux.ogg" && gameState.level !== 20) {
+        switchSong("chillFlux.ogg");
+        currentSong = "chillFlux.ogg";
+    }
 
-  if (gameState.victory === 1 && currentSfx !== "victory.ogg") {
-    currentSfx = "victory.ogg";
-  }
+    if (gameState.victory === 1 && currentSfx !== "victory.ogg") {
+        currentSfx = "victory.ogg";
+    }
 }, 100);
